85d3c8b129d2cf48eee2195170ed36045814a719
Dainius Cizikas <dainius.cizikas@hotmail.com>
2026-02-26 14:38:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+def get_tickers():
+    print("Get Tickers")
+
+def calculate_returns():
+    print("Calculate Returns")
+
+def create_covariance_matrix():
+    print("Create Covariance Matrix")
+
+def mean_variance_optimization():
+    print("Mean Variance Optimization")
+
+def in_sample_back_test():
+    print("In sample Back Test")
+
+def create_visualization():
+    print("Create Visualization")
+
+
+
+if __name__ == '__main__':
+    get_tickers()
+    calculate_returns()
+    create_covariance_matrix()
+    mean_variance_optimization()
+    in_sample_back_test()
+    create_visualization()
