5b9b3926c5d2114dac0e0d5fb8f38db87bcfe54a
Dainius Cizikas <dainius.cizikas@hotmail.com>
2026-03-05 07:58:06 +0100

    Create the calculate_returns() function

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
+import numpy as np
 import yfinance as yf
+import matplotlib.pyplot as plt
 import pandas as pd
+from tabulate import tabulate
 
 def get_inputs():
-    inputs = input("Input the comma delimited ticker symbols:\n").split()
+    target_tickers = input("Input the comma delimited ticker symbols:\n").split(',')
     start_date = input("Input the start date (yyyy-mm-dd):\n")
     end_date = input("Input the end date (yyyy-mm-dd):\n")
-    tickers = []
 
-    for i, ticker in enumerate(inputs):
-        ticker = yf.Ticker(inputs[i])
-        tickers.append(ticker)
+    #tickers = []
+    #for i, ticker in enumerate(target_tickers):
+    #    ticker = yf.Ticker(target_tickers[i])
+    #    tickers.append(ticker)
 
     #Create ticker validation and remove the invalid ones - additional assignment
+    #Check the validity of the dates - additional assignment
+    #Make the target_tickers with .strip() - additional assignment
 
-    return tickers, start_date, end_date
+    return target_tickers, start_date, end_date
 
+def calculate_returns(tickers, start, end):
+    ticker_list = tickers
+    start_date = start
+    end_date = end
 
-def calculate_returns(tickers, start_date, end_date):
-    print("Calculate Returns")
+    print(type(ticker_list))
+    print(type(start_date))
+    print(type(end_date))
+    df_data = yf.download(ticker_list, start_date, end_date, auto_adjust=False)
+
+    adj_close_prices = df_data['Adj Close']
+
+    log_returns = np.log(adj_close_prices / adj_close_prices.shift(1))
+
+    return log_returns
 
 def create_covariance_matrix():
     print("Create Covariance Matrix")
@@ -32,10 +49,11 @@ def create_visualization():
     print("Create Visualization")
 
 
-
 if __name__ == '__main__':
-    get_inputs()
-    calculate_returns()
+    input_tickers, input_start_date, input_end_date = get_inputs() #Add type hints?
+
+    adj_log_returns = calculate_returns(input_tickers, input_start_date, input_end_date) #Add type hints?
+
     create_covariance_matrix()
     mean_variance_optimization()
     in_sample_back_test()
